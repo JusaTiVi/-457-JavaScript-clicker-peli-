ec245c4d13ffb81b1b1d9ae21dc58fead3ff22b5
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-05-07 12:41:50 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-pisteet = 0
-rikki = 0
-tapsToBreak = 3
+//damage dealt to the current box + amount of damage dealt per click
+damage = 0
+breakspd = 1
+
+bounty = 0
+bountymult = 1
+boxhp = 3
+
+//shop item base prices
+morehp = 5
+document.getElementById("strongerbox").textContent = morehp
+tooldmg = 15
+document.getElementById("strongertools").textContent = tooldmg
+hammercost = 50
+document.getElementById("autohammer").textContent = hammercost
+
+//autohammer stats
+hammerspd = 0
+hammerdmg = 0
 
 //pixel art correction
 const canvas = document.getElementById("GameCanvas");
@@ -17,15 +33,50 @@ img.src = "woodenbox.png";
 
 img.onload = () => {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-
-
 }
+
+//starting bounty
+document.getElementById("currency").textContent = Math.round(bounty * 10) / 10
+
 function nappi() {
-    pisteet += 1
-    if (pisteet === tapsToBreak) {
-        pisteet = 0
-        rikki += 1
-        document.getElementById("rikotut").textContent =
-        rikki
+    canvas.classList.add("shrink");
+    setTimeout(() => {
+        canvas.classList.remove("shrink");
+    }, 140);
+
+    //calculates the amount of damage required and dealt + how much bounty is granted
+    damage += breakspd
+    if (damage >= boxhp) {
+        damage = 0
+        bounty += bountymult
+        document.getElementById("currency").textContent = Math.round(bounty * 10) / 10
+    }
+}
+//makes the box stronger, in turn returning a greater bounty
+function strbox() {
+    if (bounty >= morehp) {
+        bounty -= morehp
+        bountymult = bountymult * 2
+        morehp = morehp * 2.2
+        boxhp = boxhp * 1.5
+        document.getElementById("strongerbox").textContent = Math.round(morehp * 10) / 10
+        document.getElementById("currency").textContent = Math.round(bounty * 10) / 10
+    }
+}
+//makes the player's main tool stronger, breaking boxes in less hits
+function bettertool() {
+    if (bounty >= tooldmg) {
+        bounty -= tooldmg
+        breakspd = breakspd * 1.3
+        tooldmg = tooldmg * 1.81
+        document.getElementById("strongertools").textContent = Math.round(tooldmg * 10) / 10
+        document.getElementById("currency").textContent = Math.round(bounty * 10) / 10
+    }
+}
+//automatically damages bose for you!
+function autohammer() {
+    if (bounty >= hammercost) {
+        bounty -= hammercost
+
     }
 }

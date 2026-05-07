829e922931a5a4da7c897ea5d1bbfd9bf12ca5bd
JusaTiVi <jusa.itkonen@esedulainen.fi>
2026-05-07 10:23:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 pisteet = 0
 rikki = 0
+tapsToBreak = 3
 
+//pixel art correction
 const canvas = document.getElementById("GameCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -9,14 +11,18 @@ ctx.msImageSmoothingEnabled = false;
 ctx.webkitImageSmoothingEnabled = false;
 ctx.mozImageSmoothingEnabled = false;
 const img = new Image();
+
+//box models
 img.src = "woodenbox.png";
 
 img.onload = () => {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+
+
 }
 function nappi() {
     pisteet += 1
-    if (pisteet === 3) {
+    if (pisteet === tapsToBreak) {
         pisteet = 0
         rikki += 1
         document.getElementById("rikotut").textContent =
